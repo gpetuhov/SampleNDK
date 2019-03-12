f15ec3dd9722c3ce50374a32367a23ac54054e16
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-12 16:18:25 +0300

    Add ifndef - endif block

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,13 @@
+// To prevent multiple includes we can wrap includes in ifndef - endif block
+#ifndef MY_INCLUDES
+#define MY_INCLUDES
+
+// <file> - provided with compiler
+// "file" - your own header files
 #include <jni.h>
 #include <string>
+
+#endif
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_gpetuhov_android_samplendk_MainActivity_stringFromJNI(
